303f20008dcb4d71350195b03497122e4f9011e8
afnetstudent <afnetstudent@gmail.com>
2021-06-10 14:57:31 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './firebaseConfig';
+import { useState } from 'react';
+
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState({});
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  var ghProvider = new firebase.auth.GithubAuthProvider();
+  const handleGoogleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+
+        var credential = result.credential;
+
+        var token = credential.accessToken;
+        var user = result.user;
+        setUser(user);
+        console.log(user);
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+
+      });
+  }
+  const handleGitHubSignIn =()=>{
+    firebase
+  .auth()
+  .signInWithPopup(ghProvider)
+  .then((result) => {
+    var credential = result.credential;
+    var token = credential.accessToken;
+    var user = result.user;
+    setUser(user)
+    console.log(user);
+  }).catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    var email = error.email;
+    var credential = error.credential;
+    console.log('error',errorMessage,errorCode,email,credential)
+  });
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}> Google Sign in</button>
+      <br />
+      <button onClick={handleGitHubSignIn}> Github Sign in</button>
+      <h2> Email : {user.email}</h2>
+      <h2> Name : {user.displayName}</h2>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCtlACjVpvnYjONP5MmLR_iTa8N3Oysq6E",
+    authDomain: "my-auth-44099.firebaseapp.com",
+    projectId: "my-auth-44099",
+    storageBucket: "my-auth-44099.appspot.com",
+    messagingSenderId: "343789286684",
+    appId: "1:343789286684:web:dc77850c87901d952e36d9"
+  };
+  export default firebaseConfig;
